c5dedf00ae4e949c417054c1d598453c31607103
akram khan <akramkhan75533@gmail.com>
2025-04-15 02:25:39 +0530

    splited code and also structed it into objects

---
+++ build/CMakeFiles/shape_detector.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/akramkhan/pine-academy/C++project/OpenCV_Shape_Detection/src/ShapeDetector.cpp" "CMakeFiles/shape_detector.dir/src/ShapeDetector.cpp.o" "gcc" "CMakeFiles/shape_detector.dir/src/ShapeDetector.cpp.o.d"
+  "/Users/akramkhan/pine-academy/C++project/OpenCV_Shape_Detection/src/main.cpp" "CMakeFiles/shape_detector.dir/src/main.cpp.o" "gcc" "CMakeFiles/shape_detector.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/shape_detector.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/shape_detector.dir/src/ShapeDetector.cpp.o"
+  "CMakeFiles/shape_detector.dir/src/ShapeDetector.cpp.o.d"
+  "CMakeFiles/shape_detector.dir/src/main.cpp.o"
+  "CMakeFiles/shape_detector.dir/src/main.cpp.o.d"
+  "shape_detector"
+  "shape_detector.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/shape_detector.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
